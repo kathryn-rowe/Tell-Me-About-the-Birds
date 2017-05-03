139b8401be23f1ff8d85107dfb68a11b7916fcf6
Kathryn Rowe <kjrowe06@gmail.com>
2017-05-03 16:42:28 -0700

    Working on testing

--- static/map-spec.js
+++ static/map-spec.js
@@ -1,15 +1,46 @@
-import secret_key
-var mapbox_api_key = secret_key.mapbox_api_key
+// import secret_key;
+// var mapbox_api_key = secret_key.mapbox_api_key;
 
-describe("My Test Suite", function () {
 
-    it("should add numbers", function () {
-        var sum = renderMap(mapbox_api_key, -121.8595805, 36.2819875, );
-        expect(sum).toBe(5);
+describe("Render correct basemap", function() {
+
+  it("The 'style' is the basemap", function() {
+    var style = 'mapbox://styles/mapbox/light-v9';
+
+    expect(style).toBe('mapbox://styles/mapbox/light-v9');
+    expect(style).not.toBe(null);
+  });
+});
+
+describe("Months contain correct months", function() {
+    it("confirms correct months", function() {
+        var months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+
+      expect(months).toContain('October');
+      expect(months).toContain('January');
+      expect(a).not.toContain('bird');
     });
+});
 
-    it("should add negative numbers", function () {
-        expect(adder(1, -1)).toBe(99);
+describe("Correct coordinates", function() {
+    it("Latitude is not longitude", function() {
+        var latitude = 32;
+        var longitude = -112;
+
+        expect(longitude).toBeLessThan(latitude);
+        expect(latitude).not.toBeLessThan(longitude);
     });
-
 });
