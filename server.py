517df906ff440228b095d69a9a9c8fee534414b0
Kathryn Rowe <kjrowe06@gmail.com>
2017-05-09 09:29:47 -0700

    Changing commit username

--- server.py
+++ server.py
@@ -419,7 +419,7 @@ if __name__ == "__main__":
     connect_to_db(app)
 
     # Use the DebugToolbar
-    # DebugToolbarExtension(app)
+    DebugToolbarExtension(app)
 
     import sys
     if sys.argv[-1] == "jstest":
